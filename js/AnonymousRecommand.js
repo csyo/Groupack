a0c484b04746a213056f8d4a01167803ff5e87a8
GroupackTeam <pomelo8121@gmail.com,shane120680@gmail.com,chenchen16888@gmail.com>
2014-05-07 14:43:09 +0800

    Anonymous Recommand added

---
+++ js/AnonymousRecommand.js
@@ -0,0 +1,31 @@
+(function ($) {	
+	var elem = document.getElementById('Rslider');
+	$('#Rslider').css( "width", $('#wrapper').width()*0.89 );	
+	/* height will be set at index.js after Rslider.remove("dom_hidden") */
+	window.RecommandSwipe = Swipe(elem, {
+		// startSlide: 4,
+		// auto: 3000,
+		// continuous: true,
+		// disableScroll: true,
+		// stopPropagation: true,
+		// callback: function(index, element) {},
+		// transitionEnd: function(index, element) {}
+	});
+	
+	$(document).on('click', '#swipe_left', function(event){
+		RecommandSwipe.prev();
+	});
+	$(document).on('click', '#swipe_right', function(event){
+		RecommandSwipe.next();
+	});
+	$(document).on('click', '#recommand_user_icon', function(event){
+		alert('已寄送邀請給該群組管理員');
+		/*
+		$('#recommand_title').html('Groupack推薦您可能與您有相同興趣的資料蒐集者');
+		$('#Rslider').addClass('dom_hidden');
+		$('#swipe_left').addClass('dom_hidden');
+		$('#swipe_right').addClass('dom_hidden');
+		$('#recommand_user_area').removeClass('dom_hidden');		
+		*/
+	});
+})(jQuery);
